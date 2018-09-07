2b748cfef0c63467f24d53c916cf575f1b81cea6
TarunIsro <43070563+TarunIsro@users.noreply.github.com>
2018-09-07 21:23:00 +0530

    First project

---
+++ github_proj.py
@@ -0,0 +1,7 @@
+print("This is my first GitHub project")
+print("Ok!! Let's do a program on even and odd numbers..")
+num = int(input("Enter a number: "))
+if (num%2)==0:
+    print("It is an even number")
+else:
+    print("It is an odd number")
